38fa3b3f5afbd8972218ab07bb03edf21f8f0f47
armdhny <ariramdhany29@gmail.com>
2018-02-03 18:04:04 +0700

    first commit

---
+++ app/src/main/java/com/ramdhany/ari/studycase/FormActivity.java
@@ -0,0 +1,71 @@
+package com.ramdhany.ari.studycase;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class FormActivity extends AppCompatActivity {
+    private static final String LOG_TAG = FormActivity.class.getSimpleName();
+
+    static final String MAKAN ="com.ramdhany.ari.studycase.extra.MAKAN";
+    static final String Jumlah ="com.ramdhany.ari.studycase.extra.Jumlah";
+    static final String Nominal ="com.ramdhany.ari.studycase.extra.Nominal";
+
+
+    EditText makan;
+    EditText jum;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+
+        makan = (EditText) findViewById(R.id.editText_main);
+        jum = (EditText) findViewById(R.id.editTextJmlh);
+    }
+
+    public void EatBus(View view) {
+        Log.d(LOG_TAG, "Button clicked!"); //cek tombol bisa berfungsi pada log
+
+
+        Intent intent = new Intent(this, FormActivity2.class);
+
+        intent.putExtra("tempat","Eatbus");
+
+        String makanan = makan.getText().toString();
+        intent.putExtra(MAKAN, makanan);
+
+        String jumlah = jum.getText().toString();
+        intent.putExtra(Jumlah,jumlah);
+
+        intent.putExtra("nominal","50000");
+
+        startActivity(intent);
+    }
+
+    public void Abnormal(View view) {
+        Log.d(LOG_TAG, "Button clicked!"); //cek tombol bisa berfungsi pada log
+
+
+        Intent intent = new Intent(this, FormActivity2.class);
+
+        intent.putExtra("tempat","Abnormal");
+
+        String makanan = makan.getText().toString();
+        intent.putExtra(MAKAN, makanan);
+
+        String jumlah = jum.getText().toString();
+        intent.putExtra(Jumlah,jumlah);
+
+        intent.putExtra("nominal","30000");
+
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/ramdhany/ari/studycase/FormActivity2.java
@@ -0,0 +1,54 @@
+package com.ramdhany.ari.studycase;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class FormActivity2 extends AppCompatActivity {
+
+    TextView makan;
+    TextView porsi;
+    TextView tempat;
+    TextView nominal;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form2);
+
+        Intent intent = getIntent();
+
+        String pTempat = intent.getStringExtra("tempat");
+        String pMakan = intent.getStringExtra(FormActivity.MAKAN);
+        String pJumlah = intent.getStringExtra(FormActivity.Jumlah);
+        String pNominal = intent.getStringExtra("nominal");
+        int jNominal = Integer.valueOf(pJumlah)*Integer.valueOf(pNominal);
+
+        tempat = (TextView) findViewById(R.id.tTempat);
+        tempat.setText(pTempat);
+
+        makan = (TextView) findViewById(R.id.Makanan);
+        makan.setText(pMakan);
+
+        porsi = (TextView) findViewById(R.id.tJumlah) ;
+        porsi.setText(pJumlah);
+
+        nominal = (TextView) findViewById(R.id.tNom);
+        nominal.setText(String.valueOf(jNominal));
+
+        if (jNominal > 65000){
+            Context context = getApplicationContext();
+            Toast toast = Toast.makeText(context, "Jangan Makan Malam disini! Kemahalan. Uangmu Sedikit", Toast.LENGTH_LONG);
+            toast.show();
+        }else {
+            Context context =getApplicationContext();
+            Toast toast = Toast.makeText(context, "Makan malam disini aja! Agak murah", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+    }
+}
